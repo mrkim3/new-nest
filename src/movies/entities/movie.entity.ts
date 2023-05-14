d04cda8679ec84956db95a0908d14a24311f53d4
”miri” <Kim.miri333@gmail.com>
2023-05-14 21:07:49 +0900

    mod

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,6 @@
+export class Movie {
+  id: number;
+  titile: string;
+  year: number;
+  generes: string[];
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (movie) {
+      throw new NotFoundException(`Movie with Id ${id} not found`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: string) {
+    this.movies.filter((movie) => movie.id !== +id);
+    return true;
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
